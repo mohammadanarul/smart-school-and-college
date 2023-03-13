8ac5949baeee0f2b6143608867d606b806038ab6
ariful4082 <ariful4082@gmail.com>
2023-03-14 00:48:03 +0600

    cotomizing dashboard page

--- src/pages/Dashboard/DSearch.jsx
+++ src/pages/Dashboard/DSearch.jsx
@@ -3,32 +3,34 @@ import React from "react";
 const DSearch = () => {
   return (
     <>
-      <label htmlFor="table-search" className="sr-only">
-        Search
-      </label>
-      <div className="relative">
-        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-          <svg
-            className="w-5 h-5 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              clipRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              //   clipRule="evenodd"
-            ></path>
-          </svg>
+      <form className="hidden lg:block lg:pl-32">
+        <label htmlFor="topbar-search" className="sr-only">
+          Search
+        </label>
+        <div className="mt-1 relative lg:w-64">
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <svg
+              className="w-5 h-5 text-gray-500"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            name="email"
+            id="topbar-search"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full pl-10 p-2.5"
+            placeholder="Search"
+          />
         </div>
-        <input
-          type="text"
-          id="table-search"
-          className="block p-2 pl-10 w-80 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search for items"
-        />
-      </div>
+      </form>
     </>
   );
 };
