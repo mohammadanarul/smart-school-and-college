44889e3067504a798977471ce33897d907a2e3cd
ariful4082 <ariful4082@gmail.com>
2023-03-14 21:12:56 +0600

    added teacher modal form

--- src/pages/Dashboard/DMain.jsx
+++ src/pages/Dashboard/DMain.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-tailwind/react";
 import React from "react";
 
 const DMain = () => {
@@ -57,120 +58,7 @@ const DMain = () => {
                 <div className="overflow-x-auto rounded-lg">
                   <div className="align-middle inline-block min-w-full">
                     <div className="shadow overflow-hidden sm:rounded-lg">
-                      <table className="min-w-full divide-y divide-gray-200">
-                        <thead className="bg-gray-50">
-                          <tr>
-                            <th
-                              scope="col"
-                              className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                            >
-                              Transaction
-                            </th>
-                            <th
-                              scope="col"
-                              className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                            >
-                              Date & Time
-                            </th>
-                            <th
-                              scope="col"
-                              className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                            >
-                              Amount
-                            </th>
-                          </tr>
-                        </thead>
-                        <tbody className="bg-white">
-                          <tr>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                              Payment from{" "}
-                              <span className="font-semibold">
-                                Bonnie Green
-                              </span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 23 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              $2300
-                            </td>
-                          </tr>
-                          <tr className="bg-gray-50">
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
-                              Payment refund to{" "}
-                              <span className="font-semibold">#00910</span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 23 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              -$670
-                            </td>
-                          </tr>
-                          <tr>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                              Payment failed from{" "}
-                              <span className="font-semibold">#087651</span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 18 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              $234
-                            </td>
-                          </tr>
-                          <tr className="bg-gray-50">
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
-                              Payment from{" "}
-                              <span className="font-semibold">Lana Byrd</span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 15 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              $5000
-                            </td>
-                          </tr>
-                          <tr>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                              Payment from{" "}
-                              <span className="font-semibold">Jese Leos</span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 15 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              $2300
-                            </td>
-                          </tr>
-                          <tr className="bg-gray-50">
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900 rounded-lg rounded-left">
-                              Payment from{" "}
-                              <span className="font-semibold">
-                                THEMESBERG LLC
-                              </span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 11 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              $560
-                            </td>
-                          </tr>
-                          <tr>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                              Payment from{" "}
-                              <span className="font-semibold">Lana Lysle</span>
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                              Apr 6 ,2021
-                            </td>
-                            <td className="p-4 whitespace-nowrap text-sm font-semibold text-gray-900">
-                              $1437
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
+                      <Button>Create Institute</Button>
                     </div>
                   </div>
                 </div>
--- src/pages/Dashboard/Teachers/Teachers.jsx
+++ src/pages/Dashboard/Teachers/Teachers.jsx
@@ -1,11 +1,30 @@
-import { Card, Typography } from "@material-tailwind/react";
+import { Button, Card, Typography } from "@material-tailwind/react";
 import React from "react";
+import { useModal } from "../../../components/Utils/useModal";
+import TeacherForm from "./TeacherForm";
 
 const DashboardTeachers = () => {
+  const { handleOpen, open, handleClose } = useModal();
   return (
     <div className="pt-8 px-4">
-      <Card>
-        <Typography variant="h2">This is Teachers Page</Typography>
+      <Card className="p-5">
+        <div className="add_teacher">
+          <Button
+            onClick={handleOpen}
+            className="bg-teal-500 hover:bg-rose-800 hover:text-white duration-500"
+            variant="gradient"
+          >
+            Admit New
+          </Button>
+          <TeacherForm
+            handleClose={handleClose}
+            handleOpen={handleOpen}
+            open={open}
+          />
+        </div>
+        <Typography className="my-4" variant="h2">
+          All Teacher is Here
+        </Typography>
       </Card>
     </div>
   );
