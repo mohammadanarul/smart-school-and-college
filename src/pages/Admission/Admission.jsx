4bbd397f30226a4ab96e8382d01d4bb2945ee791
mohammadanarul <mdanarul7075@gmail.com>
2023-03-16 21:03:32 +0600

    formik form addedd.

--- src/pages/Admission/Admission.jsx
+++ src/pages/Admission/Admission.jsx
@@ -1,124 +1,120 @@
-import { Button, Checkbox, Typography } from "@material-tailwind/react";
+import { Card, Input } from "@material-tailwind/react";
+import { Formik } from "formik";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { Footer } from "../../components/FooterArea/FooterArea";
 import Navigation from "../../components/Navbar/Navigation";
-
 
 const Admission = () => {
   const [admit, setAdmit] = useState(true);
   const navigate = useNavigate();
   return (
-
     <>
       <Navigation />
 
-      <div className="container mx-auto">
-        <Typography variant="h2">Admission Rules and Regulation</Typography>
-        <div className="grid grid-cols-2 gap-4">
-          <div className="py-5">
-            <Typography variant="h3" className="border-bottom-1 border">
-              Requirement Rules
-            </Typography>
-            <ul className="list-disc">
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-            </ul>
-          </div>
-          <div className="py-5">
-            <Typography variant="h3" className="border border-bottom-10 ">
-              Optional Rules
-            </Typography>
-            <ul>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-              <li>
-                Now this is a story all about how, my life got flipped-turned
-                upside down
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="admission-area my-5">
-          <Checkbox
-
-            className="focus:border-teal-600 target:border-teal-600 focus:outline-0 border-spacing-0"
-
-            onChange={() => setAdmit(!admit)}
-            label={
-              <Typography
-                variant="small"
-                color="gray"
-                className="flex items-center font-normal"
-              >
-                I agree to this Rules & Regulations
-              </Typography>
+      <Card>
+        <h1>Anywhere in your app!</h1>
+        <Formik
+          initialValues={{
+            full_name: "",
+            phone_number: "",
+            email: "",
+            father_name: "",
+            mother_name: "",
+            gender: "",
+            religion: "",
+            blood_group: "",
+            date_of_birth: "",
+            present_address: "",
+            permanent_address: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
             }
-
-            containerProps={{ className: "-ml-2.5" }}
-
-          />
-          <br />
-
-          <Button
-            className="bg-teal-500"
-            disabled={admit ? true : false}
-            onClick={() => navigate("/admission/admission-form")}
-          >
-            Admit Now
-          </Button>
-        </div>
-      </div>
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.full_name}
+                size="lg"
+                label="First Name"
+              />
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.phone_number}
+                size="lg"
+                label="Phone Number"
+              />
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                size="lg"
+                label="Email Address"
+              />
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.father_name}
+                size="lg"
+                label="Father Name"
+              />
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.mother_name}
+                size="lg"
+                label="Mother Name"
+              />
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.gender}
+                size="lg"
+                label="Gender"
+              />
+              <Input
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.religion}
+                size="lg"
+                label="Religion"
+              />
+              {errors.email && touched.email && errors.email}
+              {errors.password && touched.password && errors.password}
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      </Card>
       <Footer />
     </>
   );
